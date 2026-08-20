082868b05a6244f4ac2bc430336e60409c2c4e37
Elbo79 <elbertthompson@gmail.com>
2026-08-19 23:27:50 -0700

    Test official UPS pay-period boundaries

--- app/src/test/java/com/pilotcompanion/app/PayPeriodCalculatorTest.java
+++ app/src/test/java/com/pilotcompanion/app/PayPeriodCalculatorTest.java
@@ -5,9 +5,17 @@ import org.junit.Test;
 import java.time.LocalDate;
 
 public class PayPeriodCalculatorTest {
-    @Test public void groupsDatesInto28DayPayPeriods() {
+    @Test public void aug20FallsInOfficialPp9() {
         var period = PayPeriodCalculator.containing(LocalDate.of(2026, 8, 20));
-        assertEquals(LocalDate.of(2026, 8, 2), period.start());
-        assertEquals(LocalDate.of(2026, 8, 29), period.end());
+        assertEquals(9, period.index());
+        assertEquals(LocalDate.of(2026, 8, 9), period.start());
+        assertEquals(LocalDate.of(2026, 9, 5), period.end());
+    }
+
+    @Test public void sep6StartsOfficialPp10() {
+        var period = PayPeriodCalculator.containing(LocalDate.of(2026, 9, 6));
+        assertEquals(10, period.index());
+        assertEquals(LocalDate.of(2026, 9, 6), period.start());
+        assertEquals(LocalDate.of(2026, 10, 3), period.end());
     }
 }

216cfc196e016be7b948987f13a84e8d14b06e61
Elbo79 <elbertthompson@gmail.com>
2026-09-03 23:10:22 -0800

    Bump Pilot Companion to v0.9.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.pilotcompanion.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 8
-        versionName = "0.8.0"
+        versionCode = 9
+        versionName = "0.9.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
